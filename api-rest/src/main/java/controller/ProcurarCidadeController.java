0653b59d7af3b28bfdcace71de104aaa702424cf
pedroangelozi25062002 <pedroangelozi25062002@gmail.com>
2022-04-19 16:29:36 -0300

    Commit 02

--- api-rest/src/main/java/controller/ProcurarCidadeController.java
+++ api-rest/src/main/java/controller/ProcurarCidadeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,7 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ch.qos.logback.classic.Logger;
 import model.AeroportoEntity;
+import service.AbrirVooService;
 import service.VoarService;
+
+@Configuration
+@ComponentScan(basePackages = { "com.api-rest.aeroporto" })
 
 @RestController
 @RequestMapping(value= "/cidade")
@@ -21,11 +27,12 @@ public class ProcurarCidadeController {
 	private static final Logger logger = (Logger) LoggerFactory.getLogger(ProcurarCidadeController.class);
 	
 	@Autowired
-	private VoarService voarservice;
+	private AbrirVooService abrirVooService;
 	
 	@RequestMapping(value= "/{cidade}", method= RequestMethod.GET)
-	public List<AeroportoEntity> procurar(@PathVariable String cidade) {
-		List<AeroportoEntity> retval = voarservice.aeroportoCidade(cidade); 
+	public List<AeroportoEntity> procurar(String cidade) {
+		List<AeroportoEntity> retval = abrirVooService.aeroportoCidade(cidade);
+		
 		if(retval != null) {
 			logger.info("Aeroporto para a cidade {}: {}", cidade, retval.size());
 		} else {
